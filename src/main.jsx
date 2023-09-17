d77e32632e2c72f9c09b2f6317e0957b4c1dbb90
narayan soni <narayansoni854@gmail.com>
2023-09-18 03:54:02 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,45 @@
+import {
+  BrowserRouter,
+  useLocation,
+  useNavigate,
+} from 'react-router-dom';
+
+import App from './App.Authentication.jsx';
+import { QueryParamProvider } from 'use-query-params';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import App from './App.jsx';
+// import App from './App.Redirect.jsx';
+// import App from './App.Nested.jsx';
+// import App from './App.Descendant.jsx';
+// import App from './App.QueryParams.jsx';
+// import App from './App.LazyLoading.jsx';
+
+// use-query-params adapeter for React Router 6
+const RouteAdapter = ({ children }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const adaptedHistory = React.useMemo(
+    () => ({
+      replace(location) {
+        navigate(location, { replace: true, state: location.state });
+      },
+      push(location) {
+        navigate(location, { replace: false, state: location.state });
+      },
+    }),
+    [navigate]
+  );
+  return children({ history: adaptedHistory, location });
+};
+
+ReactDOM.render(
+  <BrowserRouter>
+    <QueryParamProvider ReactRouterRoute={RouteAdapter}>
+      <App />
+    </QueryParamProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
